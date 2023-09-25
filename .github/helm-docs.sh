3d57eec73f6235f5421cd0456f8b7cbeb8e249fa
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2023-09-25 09:11:03 +0200

    Changed: bumop up version

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -6,7 +6,7 @@ if [[ -z "$CHART_DIRS" ]]; then
     exit 1
 fi
 
-HELM_DOCS_VERSION="1.11.0"
+HELM_DOCS_VERSION="1.11.2"
 
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz helm-docs
